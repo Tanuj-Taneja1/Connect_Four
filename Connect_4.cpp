5e979c8d98e8948943d6051d0f8b0556761d663a
tanuj_taneja <tanuj.taneja.1206@gmail.com>
2023-12-13 00:30:31 +0530

    ADD : Wincheck function and Player class

--- Connect_4.cpp
+++ Connect_4.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 class Grid{
 private:
-    int rows;
-    int columns;
+    int rowCount;
+    int columnCount;
+    int connectN;
     vector<vector<string>> grid;
 
 public:
-    Grid(int rows,int columns){
-        this->rows=rows;
-        this->columns=columns;
-        this->grid=vector<vector<string>>(rows,vector<string>(columns,"EMPTY"));
+    Grid(int rowCount,int columnCount,int connectN=4){
+        this->rowCount=rowCount;
+        this->columnCount=columnCount;
+        this->connectN=connectN;
+        this->grid=vector<vector<string>>(rowCount,vector<string>(columnCount,"EMPTY"));
     }
 
-    int placePiece(int column,string piece){
-        int lowest_row=this->rows-1;
-        for (int row=lowest_row;row>=0;row--){
+    int addPiece(int column,string piece){
+        int lowestRow=this->rowCount-1;
+        for (int row=lowestRow;row>=0;row--){
             if (this->grid[row][column]=="EMPTY"){
                 this->grid[row][column]=piece;
                 return row;
@@ -24,5 +26,84 @@ public:
         }
         return -1;
     }
-    
+    bool winCheck(int curRow,int curCol,string piece){
+        int count=0;
+        int leftCol=max(0, curCol-connectN+1);
+        int rightCol=max(this->columnCount, curCol+connectN-1);
+        int leftRow=max(0, curRow-connectN+1);
+        int rightRow=(this->rowCount, curRow+connectN-1);
+        int pieceCount=0;
+
+        for (int col=leftCol;col<=rightCol;col++){
+            string curPiece=this->grid[curRow][col];
+            if (curPiece==piece){
+                pieceCount++;
+            } 
+            else {
+                pieceCount=0;
+            }
+            if (pieceCount==connectN){
+                return true;
+            }
+        }
+
+        for (int row=leftRow;row<=rightRow;row++){
+            string curPiece=this->grid[row][curCol];
+            if (curPiece==piece){
+                pieceCount++;
+            } 
+            else {
+                pieceCount=0;
+            }
+            if (pieceCount==connectN){
+                return true;
+            }
+        }
+
+        for (int row=leftRow;row<=rightRow;row++){
+            int col=curCol-(curRow-row);
+            string curPiece=this->grid[row][col];
+            if (curPiece==piece){
+                pieceCount++;
+            } 
+            else {
+                pieceCount=0;
+            }
+            if (pieceCount==connectN){
+                return true;
+            }
+        }
+        for (int row=leftRow;row<=rightRow;row++){
+            int col=curCol+(curRow-row);
+            string curPiece=this->grid[row][col];
+            if (curPiece==piece){
+                pieceCount++;
+            } 
+            else {
+                pieceCount=0;
+            }
+            if (pieceCount==connectN){
+                return true;
+            }
+        }
+        return false;        
+    }  
+};
+
+class Player {
+private:
+    string name;
+    string piece;
+
+public:
+    Player(string name, string piece) {
+        this->name = name;
+        this->piece = piece;
+    }
+    string getName() {
+        return this->name;
+    }
+    string getPieceColor() {
+        return this->piece;
+    }
 };
